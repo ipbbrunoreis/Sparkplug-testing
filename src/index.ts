954e316c264ecc1518b5b2ff2f7a6df6a9290510
Bruno Miguel Cavaleiro Reis <103128190+ipbbrunoreis@users.noreply.github.com>
2023-03-01 15:48:19 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-import sparkplug, {ISparkplugClientOptions}  from '@nortech/sparkplug-client';
+import { ISparkplugClientOptions, newClient } from '@nortech/sparkplug-client';
+import { SparkplugClient } from 'sparkplug-client';
+import fs from 'fs';
+import { getNodeBirth } from './controller/payload.controller';
+import { IClient } from './models/Client.interface';
+import { IStation } from './models/Station.interface';
 
+const testClient: IClient = {
+  _id: '62b18fbeeddb2020f8bc56e6',
+  name: 'UTE',
+  description: 'Test Client',
+};
+
+const testStation: IStation = {
+  _id: '62b18fbeeddb2020f8bc56e7',
+  name: 'StationUTE',
+  description: 'Test Station',
+  client: testClient,
+};
+
+function startSparkplug(): void {
   const options: ISparkplugClientOptions = {
     serverUrl: `${process.env.API_EPO_URL}:${process.env.SSL_PORT}`,
     clientId: testClient._id,
@@ -21,23 +40,25 @@ import sparkplug, {ISparkplugClientOptions}  from '@nortech/sparkplug-client';
       protocolVersion: Number(process.env.SOURCE_TYPE_VERSION),
       rejectUnauthorized: false,
     },
-} 
+  };
 
-const client = sparkplug.newClient(options);
+  const client = newClient(options);
 
-client.on('birth', () => {
-   client.publishNodeBirth(getNodeBirth(), null);
-});
+  client.on('birth', () => {
+    client.publishNodeBirth(getNodeBirth(), null);
+  });
 
-client.on('connection', () => {
-   console.log('Client is connecting!!');
-});
+  client.on('connection', () => {
+    console.log('Client is connecting!!');
+  });
 
-client.on('error', (error) => {
-   console.error('Client receive error:', error);
-});
+  client.on('error', (error) => {
+    console.error('Client receive error:', error);
+  });
 
-client.on('close', () => {
-   console.log("Client receive 'close' event!!!");
-  // client.stop();
-});
+  client.on('close', () => {
+    console.log("Client receive 'close' event!!!");
+    // client.stop();
+  });
+}
+export default { startSparkplug };
